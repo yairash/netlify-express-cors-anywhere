f8e71dd4280aa38f13741c9729071f4c832dd5a4
yairash <yairash13@gmail.com>
2023-05-19 00:33:32 +0300

    Fixed proxy response

--- express/server.js
+++ express/server.js
@@ -25,6 +25,8 @@ router.get('/', (req, res) => {
 router.get('/proxy/:proxyUrl*', (req, res) => {
   req.url = req.url.replace('/proxy/', '/'); // Strip '/proxy' from the front of the URL, else the proxy won't work.
   proxy.emit('request', req, res);
+  res.sendStatus(200);
+  res.end()
 });
 
 // router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
